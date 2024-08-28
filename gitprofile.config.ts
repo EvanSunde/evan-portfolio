67aa1b607dd3e67fd012c142ae3c1d534433a73b
EvanSunde <evansunde2@gmail.com>
2024-08-28 11:13:46 +0545

    Project link update

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['evansunde/CheersAI-backend', 'evansunde/nephara-frontend', 'evansunde/chatapp'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['evansunde/CheersAI-backend', 'evansunde/skincare-frontend', 'evansunde/chatapp'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
