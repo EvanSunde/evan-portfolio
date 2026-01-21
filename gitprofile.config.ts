1c4cb9e62867ab03f9a8f257c30944b25eddd1bc
Evan Sunde <evansunde2@gmail.com>
2026-01-21 23:20:11 +0545

    typo fix

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -29,7 +29,7 @@ const CONFIG = {
       },
     },
     external: {
-      header: 'My Projects',
+      header: 'My Experience',
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
